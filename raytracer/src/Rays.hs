0907332ef1b04d56a073dbadd5f7047411b7f600
alan-windows-sp3 <alan.n.dao@gmail.com>
2015-09-26 01:52:59 -0700

    refactored record syntax pattern matching

--- raytracer/src/Rays.hs
+++ raytracer/src/Rays.hs
@@ -1,4 +1,7 @@
-module Rays where
+module Rays (
+  ray
+)
+where
 
 import Linear.Metric
 import Linear.V3
@@ -6,14 +9,22 @@ import Shapes
 
 kEpsilon = 0.000001
 
+--rayDirection will always be normalized to 1.
 data Ray = Ray { rayOrigin :: V3 Double
                , rayDirection :: V3 Double }
+               deriving (Show)
+ray :: V3 Double -> V3 Double -> Ray
+ray orig dir = Ray {rayOrigin = orig, rayDirection = (normalize dir)}
 
 hitDistances :: Ray -> Shape -> Maybe [Double]
 -- a plane should only return one hit point.
-hitDistances ray plane@(Plane _ _)
-    | isInfinite t = Nothing
+hitDistances (Ray {rayOrigin = o, rayDirection = l})
+  (Plane {planePoint = p, planeNormal = n})
+    | isInfinite t = Just [kEpsilon]
     | t < kEpsilon = Nothing
     | otherwise = Just [t]
-    where t = (dot ((planePoint plane) - (rayOrigin ray)) (planeNormal plane)) /
-              (dot (rayDirection ray) (planeNormal plane))
+    where t = (dot (p - o) n) /
+              (dot l n)
+--hitDistances ray sphere@(Sphere _ _)
+hitDistances (Ray {rayOrigin = o, rayDirection = l})
+ (Sphere {sphereCenter = c, sphereRadius = r}) = undefined
