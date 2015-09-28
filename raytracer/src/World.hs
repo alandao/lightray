9db0c4ad1ad1b249dc69271f471da403dc121587
alan-windows-sp3 <alan.n.dao@gmail.com>
2015-09-27 22:54:16 -0700

    Initial commit of new module

---
+++ raytracer/src/World.hs
@@ -0,0 +1,9 @@
+module World where
+-- ^The 'World' module deals with everything required
+-- to render one image. We construct a world, modify
+-- it by adding new objects or lights to it,
+-- and render it to an image or screen.
+
+
+
+--render :: World -> Image
