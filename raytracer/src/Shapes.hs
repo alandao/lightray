9c9edddd1ed6c4ac4e575e24debff70068d16254
alan-windows-sp3 <alan.n.dao@gmail.com>
2015-09-27 22:53:28 -0700

    renamed these modules

--- raytracer/src/Shapes.hs
+++
@@ -1,8 +0,0 @@
-module Shapes where
-
-import Linear.V3
-
-data Shape =    Plane   { planePoint    :: V3 Double
-                        , planeNormal   :: V3 Double }
-            |   Sphere  { sphereCenter  :: V3 Double
-                        , sphereRadius :: Double }
